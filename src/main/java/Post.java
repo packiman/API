f27dd071f74cfd809efb4b91e2c86ad1e6435dc3
EvgenyKlishin <klishinev@gmail.com>
2022-10-31 12:19:56 +0300

    Second commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; //поле для паспорта
+    public String patromymic;  //поле отчества
+    public String phone;  //поле номера
+    public String surname;  //поле фамилии
+    public String subscription; // поле верно или нет
+    public FormDate birthday; // поле даты рождения
+}
